bf5abfc75491ef47d0d1e43c63a9de18e9bc2fe7
Chris Arnoult <chris.arnoult@gmail.com>
2018-03-21 23:03:37 -0700

    Initial Commit

---
+++ Random Flags/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Random Flags
+//
+//  Created by Stoutbeard on 3/21/18.
+//  Copyright © 2018 Stoutbeard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
