1c9507732562ec32e20f27552643ba951536b639
Chris Arnoult <chris.arnoult@gmail.com>
2018-03-22 16:57:29 -0700

    Built out much of the programs viewcontroller.swift

--- Random Flags/ViewController.swift
+++ Random Flags/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Stoutbeard. All rights reserved.
 //
 
+import GameplayKit
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,9 +14,47 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us", ]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        askQuestion()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func askQuestion() {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer {
+            title = "Correct!"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
     }
 
     override func didReceiveMemoryWarning() {
